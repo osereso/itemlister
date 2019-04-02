1c324b5ca0e931c4d1180fd1cc0e6842d5c1d41c
mbp <mbp@MBP-15-srs.local>
2019-04-02 13:24:20 +0200

    functions for row: press, render

--- App.js
+++ App.js
@@ -1,9 +1,16 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {
+  Platform, 
+  StyleSheet, 
+  Text, 
+  View,
+  ListView,
+  TouchableHighlight} 
+  from 'react-native';
 import Toolbar from './src/components/Toolbar/Toolbar';
 import AddButton from './src/components/AddButton/AddButton';
 
-const styles = './src/style';
+const styles = require('./src/components/style');
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -12,12 +19,58 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-
 export default class App extends Component {
+  constructor(){
+    super();
+    let ds = new ListView.DataSource({rowHasChanged:(r1, r2) => r1 !== r2});
+    this.state = {
+      itemDataSource: ds
+    }
+
+    this.renderRow = this.renderRow.bind(this);
+    this.pressRow = this.pressRow.bind(this);
+  }
+
+  componentWillMount(){
+    this.getItems();
+  }
+
+  componentDidMount(){
+    this.getItems();
+  }
+
+  getItems(){
+    let items = [{title:'Item One'},{title:'Item Two'}];
+
+    this.setState({
+      itemDataSource: this.state.itemDataSource.cloneWithRows(items)
+    });
+  }
+
+  pressRow(item){
+    console.log(item);
+  }
+
+  renderRow(item){
+    return (
+      <TouchableHighlight onPress={() => {
+        this.pressRow(item);
+      }}>
+        <View style={styles.li}>
+          <Text style={styles.liText}>{item.title}</Text>
+        </View>
+      </TouchableHighlight>
+    );
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Toolbar title='itemlister' />
+        <Toolbar title="ItemLister" />
+        <ListView
+          dataSource={this.state.itemDataSource}
+          renderRow={this.renderRow}
+        />
       </View>
     );
   }
--- src/components/style.js
+++ src/components/style.js
@@ -6,24 +6,47 @@ const constants = {
 }
 
 module.exports = StyleSheet.create({
+    container: {
+      backgroundColor: '#f2f2f2',
+      flex: 1,
+    },
+    listview: {
+      flex: 1,
+    },
+    li: {
+      backgroundColor: '#fff',
+      borderBottomColor: '#eee',
+      borderColor: 'transparent',
+      borderWidth: 1,
+      paddingLeft: 16,
+      paddingTop: 14,
+      paddingBottom: 16,
+    },
+    liContainer: {
+      flex: 2,
+    },
+    liText: {
+      color: '#333',
+      fontSize: 16,
+    },
     navbar: {
-        alignItems: 'center',
-        backgroundColor: '#fff',
-        borderBottomColor: '#eee',
-        borderColor: 'transparent',
-        borderWidth: 1,
-        justifyContent: 'center',
-        height: 44,
-        flexDirection: 'row'
-      },
-      navbarTitle: {
-        color: '#444',
-        fontSize: 16,
-        fontWeight: "500"
-      },
-      toolbar: {
-        backgroundColor: '#fff',
-        height: 22,
-      }
-
-})
+      alignItems: 'center',
+      backgroundColor: '#fff',
+      borderBottomColor: '#eee',
+      borderColor: 'transparent',
+      borderWidth: 1,
+      justifyContent: 'center',
+      height: 44,
+      flexDirection: 'row'
+    },
+    navbarTitle: {
+      color: '#444',
+      fontSize: 16,
+      fontWeight: "500"
+    },
+    toolbar: {
+      backgroundColor: '#fff',
+      height: 22,
+    }
+  });
+  
